7f2b8e00aae1e01a08f0e5612d890f552f9a4116
Ethan Qiu <qethan1996@gmail.com>
2023-05-25 11:33:02 -0400

    finished the all the pages now work on styling

--- scripts/mountains.js
+++ scripts/mountains.js
@@ -1,13 +1,42 @@
-const searchingMoun = document.querySelector("#mountain-DropList");
-const displayDiv = document.querySelector("#display-div");
+const mountainDropList = document.querySelector("#mountains-DropList");
+const mountainTblBody = document.querySelector("#mountain-tbl-body");
 
+function buildMountainRow(tbody, mountain) {
+  let row = tbody.insertRow(-1);
 
-window.onload = function loadMountains() {
-  for (let i = 0; i < mountainsArray.length; i++) {
-    let mountainOption = document.createElement("option");
-    mountainOption.value = i;
-    mountainOption.textContent = mountainsArray[i].name;
-    mountainSearch.appendChild(mountainOption);
+  let cell1 = row.insertCell(0);
+  cell1.innerText = mountain.name;
+
+  let cell2 = row.insertCell(1);
+  cell2.innerText = mountain.desc;
+
+  let cell3 =  row.insertCell(2);
+  cell3.innerText = mountain.elevation;
+
+  let cell4 =  row.insertCell(3);
+  cell4.innerText = mountain.effort;
+
+  let cell5 =  row.insertCell(4);
+  cell5.innerHTML = `<img src='images/${mountain.img}'>`;
+}
+
+function displayMountainDropList() {
+  mountainDropList.innerHTML = "";
+  for (const mountain of mountainsArray) {
+    let option = new Option(mountain.name, mountain.name);
+    mountainDropList.appendChild(option);
   }
-};
+}
+displayMountainDropList();
+
+function displayMountainInfo() {
+  mountainTblBody.innerHTML = "";
+
+let mountainInfo = mountainsArray.find(function(mountain) {
+    return mountainDropList.value == mountain.name
+}) 
+buildMountainRow(mountainTblBody, mountainInfo);
+
+}
+displayMountainInfo();
 
